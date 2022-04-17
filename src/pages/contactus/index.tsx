22e6c5d3aea9eb1c6d1d5147fd56eba6e599aa71
bsanty-santhosh <89247020+bsanty-santhosh@users.noreply.github.com>
2022-04-17 20:52:56 +0530

    Forms Netlify

--- src/pages/contactus/index.tsx
+++ src/pages/contactus/index.tsx
@@ -5,10 +5,11 @@ import Grid from '@mui/material/Grid'
 import DatePickerWrapper from 'src/@core/styles/libs/react-datepicker'
 
 // ** Demo Components Imports
-// import FormLayoutsBasic from 'src/views/form-layouts/FormLayoutsBasic'
-import FormLayoutsIcons from 'src/views/form-layouts/FormLayoutsIcons'
 // import FormLayoutsSeparator from 'src/views/form-layouts/FormLayoutsSeparator'
 // import FormLayoutsAlignment from 'src/views/form-layouts/FormLayoutsAlignment'
+// import FormLayoutsBasic from 'src/views/form-layouts/FormLayoutsBasic'
+
+import FormLayoutsIcons from 'src/views/form-layouts/FormLayoutsIcons'
 
 // ** Third Party Styles Imports
 import 'react-datepicker/dist/react-datepicker.css'
